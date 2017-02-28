eb678f9cb7b754b075bf6b16c108e02246c157dc
AresPeng <jue2yin81991@163.com>
2017-02-28 09:09:12 +0800

    Initial commit

---
+++ Example/podTestLibrary/APAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APAppDelegate.h
+//  podTestLibrary
+//
+//  Created by AresPeng on 02/28/2017.
+//  Copyright (c) 2017 AresPeng. All rights reserved.
+//
+
+@import UIKit;
+
+@interface APAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/podTestLibrary/APViewController.m
@@ -0,0 +1,29 @@
+//
+//  APViewController.m
+//  podTestLibrary
+//
+//  Created by AresPeng on 02/28/2017.
+//  Copyright (c) 2017 AresPeng. All rights reserved.
+//
+
+#import "APViewController.h"
+
+@interface APViewController ()
+
+@end
+
+@implementation APViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
